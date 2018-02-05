4d0598f41af175392961c23ace127d63d9dfe37f
gkhasel1 <gkhasel1@gmail.com>
2018-02-05 11:19:34 -0500

    migrations

--- scripts/migrate-students.py
+++ scripts/migrate-students.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+from datetime import date
 
 from pymongo import MongoClient
 
@@ -23,17 +24,71 @@ columns = [
     "If your child has previously participated in KCTL, please identify his/her team."
 ]
 
+client = MongoClient("mongodb://localhost:27017")
+db = client.kctl
+
+def calculate_age(year, month, day):
+    today = date.today()
+    return today.year - year - ((today.month, today.day) < (month, day))
+
+def site_bucket(dob):
+    print(dob)
+    dob_l = dob.split("-")
+    year = int(dob_l[2])
+    month = int(dob_l[1])
+    day = int(dob_l[0])
+    age = calculate_age(year, month, day)
+    if age < 10:
+        return "under-10"
+    elif age >= 10:
+        return "over-10"
+
+#print(client.database_names())
+
+cursor = db.students.find({})
+print("ok")
+for document in cursor:
+    print("wow")
+    print(document)
+
 with open(filename) as f:
     for i, line in enumerate(f):
-        print("line {0} = {1}".format(i, line.split(",")))
-        student = {
-            "season": season,
-            "program": program,
-            # site
-            "parentEmail": line[1],
-            "firstName": line[2].split()[:-1],
-            "lastName": line[2].split()[-1],
-            # address
-            "dob": line[4].replace("/", "-"),
+        if i == 0:
+            continue
+        #print("line {0} = {1}".format(i, line.split(",")))
+        line = line.split("\t")
+        try:
+            student = {
+                "season": season,
+                "program": program,
+                "site": site_bucket(line[4].replace("/", "-")),
+                "parentEmail": line[1],
+                "firstName": " ".join(line[2].split()[:-1]),
+                "lastName": line[2].split()[-1],
+                "parentAddress": line[3],
+                "dob": line[4].replace("/", "-"),
+                "parentPrimaryPhone": line[6],
+                "emergencyName": line[8],
+                "emergencyPhone": line[9],
+                "uniform": line[10],
+                "allergy": line[11],
+                "previousParticipation": line[12]
+            }
+            print(student)
+        except:
+            print("FAILFAILFAIL")
+            student = {
+                "season": season,
+                "program": program,
+                "site": "over-10",
+                "firstName": "Jonathan",
+                "lastName": "Del Rosario"
+            }
+            print(student)
+            print(line)
 
-        }
+        try:
+            db.students.insert(student)
+        except:
+            print("WRITEFAIL")
+            print(student)
